c2da1b2a8cc8d8f02870d7d857d43705e57b26a4
Alexander Nunez <xanderve@gmail.com>
2017-05-10 17:06:20 -0400

    Done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-//Alexander Nunez
 
-// 1. You're tasked with creating some software that should be able to calculate the area of a rectangle. Write a single function that accepts 2 integer parameters and return the calculated area as an integer. In your Main function, call your area function passing in same sample data to test that your function works. You will be required to print the result of your calculation to the screen.
 namespace area
 {
     class Program
@@ -21,7 +19,6 @@ namespace area
 
             Console.WriteLine("The area of the rectangle is: "+Area);            
         }
-
         private static int area(int width, int height)
         {
             return (width * height);
