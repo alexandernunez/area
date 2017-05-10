b8c301690bb3d60632208db90f9e43b18658d69b
Alexander Nunez <xanderve@gmail.com>
2017-05-10 12:19:23 -0400

    Done

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace area
             Console.WriteLine("Please input the height of the rectangle (without decimals)");
             int height = int.Parse(Console.ReadLine());
             
+            //This calls the area function
             int Area = area(width, height);
 
             Console.WriteLine("The area of the rectangle is: "+Area);            
